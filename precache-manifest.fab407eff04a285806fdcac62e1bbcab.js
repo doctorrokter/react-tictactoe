c651dc776b78de17973d4f6e92b69f1638ff4881
Mikhail Chachkouski <doctorrokter@gmail.com>
2019-04-15 08:49:33 +0300

    Updates

---
+++ precache-manifest.fab407eff04a285806fdcac62e1bbcab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baf581c95e77173fb80d",
+    "url": "/react-tictactoe/static/js/runtime~main.d9b493ca.js"
+  },
+  {
+    "revision": "c54791f3a2e264b77ad9",
+    "url": "/react-tictactoe/static/js/main.5b7c56ae.chunk.js"
+  },
+  {
+    "revision": "2faef6bc8214d1135160",
+    "url": "/react-tictactoe/static/js/2.f09c5d3d.chunk.js"
+  },
+  {
+    "revision": "c54791f3a2e264b77ad9",
+    "url": "/react-tictactoe/static/css/main.63a94f4a.chunk.css"
+  },
+  {
+    "revision": "d798337f84b3ff97930083dd45356638",
+    "url": "/react-tictactoe/index.html"
+  }
+];
